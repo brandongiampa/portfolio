16d46bf7eb38d0be096d9987628cbae218072a83
brandongiampa <30915668+brandongiampa@users.noreply.github.com>
2020-05-24 22:27:13 -0700

    fix links on portfolio page

--- portfolio.php
+++ portfolio.php
@@ -57,9 +57,9 @@
         </a>
         <div class="work-overlay">
           <div class="work-links">
-            <a href="work.php?work=<?php echo $row['link'];?>" class="btn"><i class="fas fa-info-circle"></i>About</a>
-            <a href="<?php echo $row['github'];?>" class="btn"><i class="fab fa-github"></i>Github</a>
-            <a href="https://brandongiampa.com/<?php echo $row['link'];?>" class="btn"><i class="fas fa-eye"></i>View</a>
+            <a href="work.php?work=<?php echo $row['link'];?>" class="btn btn-transparent"><i class="fas fa-info-circle"></i>About</a>
+            <a href="<?php echo $row['github'];?>" class="btn btn-transparent"><i class="fab fa-github"></i>Github</a>
+            <a href="https://brandongiampa.com/<?php echo $row['link'];?>" class="btn btn-transparent"><i class="fas fa-eye"></i>View</a>
           </div>
         </div>
       </div>
