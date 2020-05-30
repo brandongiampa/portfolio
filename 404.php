fca761f446e92dd97d2c6dd1d9451bd2ac416772
brandongiampa <30915668+brandongiampa@users.noreply.github.com>
2020-05-29 17:11:16 -0700

    beautify links (missed this file first time)

--- 404.php
+++ 404.php
@@ -1,4 +1,3 @@
-
 <?php include_once 'includes/header.php';?>
 <div class="hero-image2 fade-in fast animated">
   <div class="hero-bg-mini" id="hero-404"></div>
@@ -8,7 +7,7 @@
   <h2>Uh-oh!</h2>
   <p>It looks like someone got lost. You just got yourself a 404 Error!</p>
   <h3>Redirecting you in <span id="redirect">5</span>...</h3>
-  <a href="index.php" class="btn btn-purple">Home</a>
+  <a href="<?php  echo $site_url;?>" class="btn btn-purple">Home</a>
 </div>
 
 <?php include_once 'includes/footer.php';?>
